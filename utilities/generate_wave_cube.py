7af712ec9c9edb4692cd81936557cc1160304b4c
Damir Wallener <damir00@Damirs-MacBook-Pro.local>
2024-03-21 13:15:49 -0700

    added multiple wave types

--- utilities/generate_wave_cube.py
+++ utilities/generate_wave_cube.py
@@ -13,6 +13,7 @@
 # read arguments
 
 import sys, argparse
+import numpy as np
 
 def return_args():
         
@@ -37,12 +38,43 @@ def return_args():
     print ("X res     : ", resX)
     print ("Y res     : ", resY)
     print ("Z res     : ", resZ)
+    print ("")
+    print ("**********************************************")
 
     return wave, ts, resX, resY, resZ
 
 
+def create_chopes_at_timestep(i):
+    print ("Chopes timestep: ", i)
+
+
+def create_pipe_at_timestep(i):
+    print ("Pipe timestep: ", i)
+
+
+# Special wave - just a constant, straight line, intended for testing/training
+def create_constant_at_timestep(i):
+    print ("Constant wave at: ", i)
+
+
+def create_wave_timestep(timestep, wave):
+
+    if wave == "chopes":
+        create_chopes_at_timestep(timestep)
+    if wave == "pipe":
+        create_pipe_at_timestep(timestep)
+    if wave == "constant":
+        create_constant_at_timestep(timestep)
+
+
+# pull instructions from command line
 wave, ts, resX, resY, resZ = return_args()
 
+# create the wave container
+this_wave = np.zeros((resX, resY, resZ))
+
+for i in range(ts):
+    create_wave_timestep(i, wave)
 
 
 
